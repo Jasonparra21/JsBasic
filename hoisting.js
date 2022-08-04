5c10e3f64f9116e89e23faf37b7275b25f5376b9
Jasonparra21 <jeissonparra1212@gmail.com>
2022-08-04 13:55:06 -0500

    Se recargan los archivos

---
+++ hoisting.js
@@ -0,0 +1,12 @@
+var pedo;
+pedo ="Pedorra";
+
+//
+console.log(miNombre);
+var miNombre = "Jei";
+
+Hey();
+
+function Hey() {
+    console.log("Hola" + miNombre);
+}
---
+++ variables.js
@@ -0,0 +1,14 @@
+var nombre = "Jeisson";
+// declarar
+var edad;
+// inicializar
+edad =30;
+
+var elementos = ["cosa", "pedo"]
+elementos[1]
+
+// objeto
+    var persona ={
+        nombre: "Jei",
+        edad:30
+    }
